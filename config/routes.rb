d82eda5edde41a20cb0b182d5506d603d06f87c4
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-04-07 03:13:10 +0200

    Testing ckeditor gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   devise_for :admins
   
   namespace :admin do
     resources :shoes
   end
   root 'admin/shoes#index'
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
